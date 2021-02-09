c116411106862d4f947ded6bb4fec626a2b87d35
dav1710 <76254008+dav1710@users.noreply.github.com>
2021-02-09 14:16:09 +0400

    Initial commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,18 @@
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" />
+            </div>
+            <div>ava+descr</div>
+            <div>My Posts</div>
+            <div>New posts</div>
+            <div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
